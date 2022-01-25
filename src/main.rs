84d01198f1f7c160bfcc567987bb1396a09e7d6e
Sönke Hahn <soenkehahn@gmail.com>
2022-01-24 23:27:18 -0500

    Only run the command if the output doesn't exist (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::io;
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
     if let Err(error) = run() {
-        eprintln!("ERROR: {}", error.0)
+        eprintln!("ERROR: {}", error.0);
+        std::process::exit(1);
     }
 }
 
+#[derive(Debug)]
 struct AppError(String);
 
 impl From<io::Error> for AppError {
@@ -17,22 +20,34 @@ impl From<io::Error> for AppError {
 
 fn run() -> Result<(), AppError> {
     let args = parse_args()?;
-    let mut command = Command::new(args.command);
-    command.args(args.args);
-    command.spawn()?.wait()?;
+    if !args.input.exists() {
+        return Err(AppError(format!(
+            "file not found: {}",
+            args.input.to_string_lossy()
+        )));
+    }
+    if !args.output.exists() {
+        let mut command = Command::new(args.command);
+        command.args(args.args);
+        command.spawn()?.wait()?;
+    }
     Ok(())
 }
 
 #[derive(Debug)]
 struct Args {
+    input: PathBuf,
+    output: PathBuf,
     command: String,
     args: Vec<String>,
 }
 
 fn parse_args() -> Result<Args, AppError> {
     let args = std::env::args();
-    let mut args = args.skip(3);
+    let mut args = args.skip(1);
     Ok(Args {
+        input: PathBuf::from(args.next().unwrap()),
+        output: PathBuf::from(args.next().unwrap()),
         command: args
             .next()
             .ok_or(AppError("not enough arguments provided".to_owned()))?,
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,18 +1,93 @@
 use anyhow::Result;
 use cradle::prelude::*;
+use pretty_assertions::assert_eq;
+use std::path::PathBuf;
 use tempfile::TempDir;
 
+struct Context {
+    temp_dir: TempDir,
+}
+
+#[derive(PartialEq, Debug)]
+struct Output {
+    stdout: String,
+    stderr: String,
+    exit_code: i32,
+}
+
+fn def() -> Output {
+    Output {
+        stdout: "".to_owned(),
+        stderr: "".to_owned(),
+        exit_code: 0,
+    }
+}
+
+impl Context {
+    fn new() -> Result<Self> {
+        let temp_dir = TempDir::new()?;
+        Ok(Context { temp_dir })
+    }
+
+    fn input(&self) -> PathBuf {
+        self.temp_dir.path().join("input")
+    }
+
+    fn output(&self) -> PathBuf {
+        self.temp_dir.path().join("output")
+    }
+
+    fn run(&self) -> Output {
+        let (StdoutTrimmed(stdout), Stderr(stderr), Status(status)) = (
+            CurrentDir(self.temp_dir.path()),
+            executable_path::executable_path("if-newer"),
+            "input",
+            "output",
+            "echo",
+            "command ran",
+        )
+            .run_output();
+        Output {
+            stdout,
+            stderr,
+            exit_code: status.code().unwrap(),
+        }
+    }
+}
+
+fn it(message: &str) {
+    eprintln!("it {}", message);
+}
+
 #[test]
-fn runs_given_commands() -> Result<()> {
-    let temp_dir = TempDir::new()?;
-    let StdoutTrimmed(output) = (
-        executable_path::executable_path("if-newer"),
-        temp_dir.path().join("file"),
-        temp_dir.path().join("output"),
-        "echo",
-        "foo",
-    )
-        .run_output();
-    assert_eq!(output, "foo");
+fn simple() -> Result<()> {
+    let context = Context::new()?;
+    it("errors when the input file doesn't exist");
+    assert_eq!(
+        context.run(),
+        Output {
+            stderr: "ERROR: file not found: input\n".to_owned(),
+            exit_code: 1,
+            ..def()
+        }
+    );
+    ("touch", context.input()).run_result()?;
+    it("runs the command when the output file doesn't exist");
+    assert_eq!(
+        context.run(),
+        Output {
+            stdout: "command ran".to_owned(),
+            ..def()
+        }
+    );
+    ("touch", context.output()).run_result()?;
+    it("doesn't run the command when the output exists");
+    assert_eq!(
+        context.run(),
+        Output {
+            stdout: "".to_owned(),
+            ..def()
+        }
+    );
     Ok(())
 }
